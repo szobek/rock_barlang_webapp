616081e962338d5800b2c125e770300a7909e9c0
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-05-21 15:51:12 +0200

    feat: refactor band card styles and remove unused logo display logic

--- resources/views/components/band-card.blade.php
+++ resources/views/components/band-card.blade.php
@@ -2,11 +2,5 @@
 
 <a href="{{ route('bands.show', $band->id) }}" class="band"  style="background: url( @if (!$band->image_path){{ asset('assets/images/band_bg_3.png') }}@else {{ $band->image_path }} @endif) no-repeat center; background-size: cover; ;" >
     <h2>{{ $band->name }}</h2>
-    <div class="logo">
-        @if ($band->image_path)
-            <img src="{{ $band->image_path }}" alt="{{ $band->name }} képe" class="band-image">
-        @else
-            <div class="no-image-placeholder"></div>
-        @endif
-    </div>
+
 </a>
