a014e93a902fd535dc597a5f04c7287c6b47463b
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-05-25 15:56:45 +0200

    feat: create album component; refactor album display logic in albums page

---
+++ resources/views/components/album.blade.php
@@ -0,0 +1,17 @@
+@props(['album'])
+<div class="album filter-card" data-album-id="{{ $album->id }}" data-name="{{ $album->name }}"
+    data-album-release-year="{{ $album->release_year }}" <a href="{{ route('album.show', $album->id) }}" class=" ">
+    <h2>{{ $album->name }}</h2>
+    <p>
+        @if ($album->release_year)
+            <span class="release-year">Megjelenés éve: {{ $album->release_year }}</span>
+        @endif
+    </p>
+    </a>
+    <div class="description">
+        <p>{{ $album->description }}</p>
+    </div>
+    @if ($album->band)
+        <p>Zenekar: <a href="{{ route('band.show', $album->band->id) }}">{{ $album->band->name }}</a></p>
+    @endif
+</div>
--- resources/views/page/albums.blade.php
+++ resources/views/page/albums.blade.php
@@ -5,22 +5,7 @@
     <div class="album-wrapper flex-container ">
 
         @foreach ($albums as $album)
-            <div class="album filter-card" data-album-id="{{ $album->id }}" data-name="{{ $album->name }}" data-album-release-year="{{ $album->release_year }}"
-                <a href="{{ route('album.show', $album->id) }}" class=" ">
-                    <h2>{{ $album->name }}</h2>
-                    <p>
-                        @if ($album->release_year)
-                            <span class="release-year">Megjelenés éve: {{ $album->release_year }}</span>
-                        @endif
-                    </p>
-                </a>
-                <div class="description">
-                    <p>{{ $album->description }}</p>
-                </div>
-                @if ($album->band)
-                    <p>Zenekar: <a href="{{ route('band.show', $album->band->id) }}">{{ $album->band->name }}</a></p>
-                @endif
-            </div>
+            <x-album :album="$album" />
         @endforeach
     </div>
 @endsection
