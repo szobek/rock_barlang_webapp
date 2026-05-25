02d431b036f076dbf8c026fa6bf4c5c669183581
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-05-25 10:11:41 +0200

    feat: add Font Awesome icons to navbar; create navbar styles and structure

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,18 @@
 <div class="bottom-nav">
 
     <a href="/bands" class="">
+        <i class="fas fa-user-friends"></i>
+
         <span class="">Zenekarok</span>
     </a>
 
     <a href="/albums" class="">
+        <i class="fas fa-compact-disc"></i>
         <span class="">Albumok</span>
     </a>
 
     <a href="/members" class="">
+        <i class="fas fa-user"></i>
         <span class="">Tagok</span>
     </a>
 
