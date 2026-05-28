771aca0e0e43136c032ed4b6d275030a20844107
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-05-28 18:34:38 +0200

    style: add blur effect to member card for improved visual depth

--- resources/views/components/member-card.blade.php
+++ resources/views/components/member-card.blade.php
@@ -1,6 +1,7 @@
 @props(['member'])
 <div class="member"
     style="background-image: url({{ $member->image_path ?? asset('assets/images/member_bg.png') }});background-size: cover; background-position: center;">
+    <div class="blur"></div>
     <h2>{{ $member->name }}</h2>
     <p><a href="{{ route('band.show', $member->band->id) }}">{{ $member->band->name }}</a></p>
 </div>
