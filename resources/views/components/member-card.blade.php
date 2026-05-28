7716483fd855220a4b3ff360a19f4885281ff483
Kunszt Norbert <kunszt.norbert@gmail.com>
2026-05-28 21:56:59 +0200

    fix: conditionally display blur effect on member card based on image presence

--- resources/views/components/member-card.blade.php
+++ resources/views/components/member-card.blade.php
@@ -1,7 +1,9 @@
 @props(['member'])
 <div class="member"
     style="background-image: url({{ $member->image_path ?? asset('assets/images/member_bg.png') }});background-size: cover; background-position: center;">
-    <div class="blur"></div>
+    @if (!$member->image_path)
+        <div class="blur"></div>
+    @endif
     <h2>{{ $member->name }}</h2>
     <p><a href="{{ route('band.show', $member->band->id) }}">{{ $member->band->name }}</a></p>
 </div>
